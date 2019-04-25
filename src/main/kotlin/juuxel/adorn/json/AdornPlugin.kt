77d00965210b9948e4f7879fd2d5ea877a9d6c5e
Juuxel <kasperi.kauppi@gmail.com>
2019-04-25 17:51:20 +0300

    Update json-factory plugin

--- src/main/kotlin/juuxel/adorn/json/AdornPlugin.kt
+++ src/main/kotlin/juuxel/adorn/json/AdornPlugin.kt
@@ -3,16 +3,10 @@ package juuxel.adorn.json
 import io.github.cottonmc.jsonfactory.gens.ContentGenerator
 import io.github.cottonmc.jsonfactory.gens.GeneratorInfo
 import io.github.cottonmc.jsonfactory.gens.block.SuffixedBlockItemModel
-import io.github.cottonmc.jsonfactory.gens.block.SuffixedBlockState
 import io.github.cottonmc.jsonfactory.gens.block.SuffixedLootTable
 import io.github.cottonmc.jsonfactory.plugin.Plugin
 
 object AdornPlugin : Plugin {
-    init {
-        // TODO: Remove this from JF, use a system similar to subcategories
-        GeneratorInfo.Categories.addCategory(AdornCategory)
-    }
-
     //adorn:red,adorn:black,adorn:green,adorn:brown,adorn:blue,adorn:purple,adorn:cyan,adorn:light_gray,adorn:gray,adorn:pink,adorn:lime,adorn:yellow,adorn:light_blue,adorn:magenta,adorn:orange,adorn:white
     //adorn:oak,adorn:spruce,adorn:birch,adorn:jungle,adorn:acacia,adorn:dark_oak
 
@@ -62,8 +56,7 @@ object AdornPlugin : Plugin {
     object AdornCategory : GeneratorInfo.Category {
         override val description = null
         override val displayName = "Adorn"
-        // TODO: only used by placeholder textures, figure out a better name for this
-        override val path = ""
+        override val placeholderTexturePath = null
     }
 
     enum class Subcategories(override val displayName: String, override val description: String? = null) : GeneratorInfo.Subcategory {
