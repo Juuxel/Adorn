61eaad143f3d47fa3a1ecc9b84cca0da6e8c2045
Juuxel <kasperi.kauppi@gmail.com>
2019-05-12 01:45:35 +0300

    Update sneak-sleeping code

--- src/main/kotlin/juuxel/adorn/lib/ModBlocks.kt
+++ src/main/kotlin/juuxel/adorn/lib/ModBlocks.kt
@@ -10,6 +10,7 @@ import net.fabricmc.api.EnvType
 import net.fabricmc.api.Environment
 import net.fabricmc.fabric.api.client.render.BlockEntityRendererRegistry
 import net.fabricmc.fabric.api.event.player.UseBlockCallback
+import net.minecraft.item.BlockItem
 import net.minecraft.util.ActionResult
 import net.minecraft.util.DyeColor
 
@@ -43,10 +44,14 @@ object ModBlocks : PolyesterRegistry(Adorn.NAMESPACE) {
     val TRADING_STATION: TradingStationBlock = registerBlock(TradingStationBlock())
 
     fun init() {
-        UseBlockCallback.EVENT.register(UseBlockCallback { player, world, _, hitResult ->
+        UseBlockCallback.EVENT.register(UseBlockCallback { player, world, hand, hitResult ->
             val state = world.getBlockState(hitResult.blockPos)
             val block = state.block
-            if (block is SofaBlock && player.isSneaking) {
+            // Check that:
+            // - the block is a sofa
+            // - the player is sneaking
+            // - the player isn't holding a block
+            if (block is SofaBlock && player.isSneaking && player.getStackInHand(hand).item !is BlockItem) {
                 block.sneakClick(state, world, hitResult.blockPos, player)
             } else ActionResult.PASS
         })
