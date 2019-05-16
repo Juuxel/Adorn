f50e7f42add7c712dc373b9a2a42c148c83f471b
Juuxel <kasperi.kauppi@gmail.com>
2019-05-16 20:36:08 +0300

    [WIP] Add extinguished stone torches

--- src/main/kotlin/juuxel/adorn/block/StoneTorchBlock.kt
+++ src/main/kotlin/juuxel/adorn/block/StoneTorchBlock.kt
@@ -2,22 +2,114 @@ package juuxel.adorn.block
 
 import io.github.juuxel.polyester.block.PolyesterBlock
 import net.fabricmc.fabric.api.block.FabricBlockSettings
-import net.minecraft.block.Blocks
-import net.minecraft.block.TorchBlock
-import net.minecraft.block.WallTorchBlock
+import net.minecraft.block.*
+import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.fluid.FluidState
+import net.minecraft.item.Items
 import net.minecraft.sound.BlockSoundGroup
+import net.minecraft.sound.SoundCategory
+import net.minecraft.sound.SoundEvents
+import net.minecraft.state.StateFactory
+import net.minecraft.state.property.Properties
+import net.minecraft.util.Hand
+import net.minecraft.util.hit.BlockHitResult
+import net.minecraft.util.math.BlockPos
+import net.minecraft.world.IWorld
+import net.minecraft.world.World
+import virtuoel.towelette.api.Fluidloggable
 
-class StoneTorchBlock : TorchBlock(settings), PolyesterBlock {
+class StoneTorchBlock : TorchBlock(settings), PolyesterBlock, Fluidloggable {
     override val name = "stone_torch"
     override val itemSettings = null
     override val hasDescription = true
 
-    class Wall(settings: Settings) : WallTorchBlock(settings), PolyesterBlock {
+    init {
+        defaultState = defaultState.with(LIT, true)
+    }
+
+    override fun appendProperties(builder: StateFactory.Builder<Block, BlockState>) {
+        super.appendProperties(builder)
+        builder.add(LIT)
+    }
+
+    override fun tryFillWithFluid(world: IWorld, pos: BlockPos, state: BlockState, fluidState: FluidState) =
+        super.tryFillWithFluid(world, pos, state, fluidState).also {
+            if (it) {
+                world.setBlockState(pos, world.getBlockState(pos).with(LIT, false), 3)
+            }
+        }
+
+    override fun getLuminance(state: BlockState): Int {
+        return if (state[LIT]) super.getLuminance(state) else 0
+    }
+
+    override fun activate(
+        state: BlockState, world: World, pos: BlockPos, player: PlayerEntity, hand: Hand, hitResult: BlockHitResult?
+    ): Boolean {
+        val stack = player.getStackInHand(hand)
+        if (!state[LIT] && stack.item == Items.FLINT_AND_STEEL) {
+            if (world.getFluidState(pos).isEmpty) {
+                world.setBlockState(pos, state.with(LIT, true))
+            } else {
+                world.playSound(
+                    pos.x.toDouble(), pos.y.toDouble(), pos.z.toDouble(),
+                    SoundEvents.BLOCK_FIRE_EXTINGUISH, SoundCategory.BLOCKS,
+                    1f, 1f, false
+                )
+            }
+
+            stack.applyDamage(1, player) {}
+            return true
+        }
+
+        return super.activate(state, world, pos, player, hand, hitResult)
+    }
+
+    class Wall(settings: Settings) : WallTorchBlock(settings), PolyesterBlock, Fluidloggable {
         override val name = "wall_stone_torch"
         override val itemSettings = null
+
+        override fun appendProperties(builder: StateFactory.Builder<Block, BlockState>) {
+            super.appendProperties(builder)
+            builder.add(LIT)
+        }
+
+        override fun tryFillWithFluid(world: IWorld, pos: BlockPos, state: BlockState, fluidState: FluidState) =
+            super.tryFillWithFluid(world, pos, state, fluidState).also {
+                if (it) {
+                    world.setBlockState(pos, world.getBlockState(pos).with(LIT, false), 3)
+                }
+            }
+
+        override fun getLuminance(state: BlockState): Int {
+            return if (state[LIT]) super.getLuminance(state) else 0
+        }
+
+        override fun activate(
+            state: BlockState, world: World, pos: BlockPos, player: PlayerEntity, hand: Hand, hitResult: BlockHitResult?
+        ): Boolean {
+            val stack = player.getStackInHand(hand)
+            if (!state[LIT] && stack.item == Items.FLINT_AND_STEEL) {
+                if (world.getFluidState(pos).isEmpty) {
+                    world.setBlockState(pos, state.with(LIT, true))
+                } else {
+                    world.playSound(
+                        pos.x.toDouble(), pos.y.toDouble(), pos.z.toDouble(),
+                        SoundEvents.BLOCK_FIRE_EXTINGUISH, SoundCategory.BLOCKS,
+                        1f, 1f, false
+                    )
+                }
+
+                stack.applyDamage(1, player) {}
+                return true
+            }
+
+            return super.activate(state, world, pos, player, hand, hitResult)
+        }
     }
 
     companion object {
+        val LIT = Properties.LIT
         internal val settings = FabricBlockSettings.copy(Blocks.TORCH)
             .lightLevel(15)
             .sounds(BlockSoundGroup.STONE)
