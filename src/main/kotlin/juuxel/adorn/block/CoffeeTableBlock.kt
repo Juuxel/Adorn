6c882127ca3f1189e24c5039ccc81c1961f76f8b
Juuxel <6596629+Juuxel@users.noreply.github.com>
2019-11-24 19:46:58 +0200

    Simplify the table block

--- src/main/kotlin/juuxel/adorn/block/CoffeeTableBlock.kt
+++ src/main/kotlin/juuxel/adorn/block/CoffeeTableBlock.kt
@@ -1,105 +1,42 @@
 package juuxel.adorn.block
 
 import juuxel.adorn.api.block.BlockVariant
+import net.minecraft.block.Block
 import net.minecraft.block.BlockState
+import net.minecraft.block.Waterloggable
+import net.minecraft.entity.EntityContext
+import net.minecraft.fluid.Fluids
+import net.minecraft.item.ItemPlacementContext
+import net.minecraft.state.StateManager
+import net.minecraft.state.property.BooleanProperty
+import net.minecraft.state.property.Properties
 import net.minecraft.util.math.BlockPos
-import net.minecraft.util.math.Direction
 import net.minecraft.util.shape.VoxelShape
 import net.minecraft.world.BlockView
 
-open class CoffeeTableBlock(variant: BlockVariant) : AbstractTableBlock(variant.createSettings().nonOpaque()) {
-    override fun canConnectTo(state: BlockState, sideOfSelf: Direction) =
-        state.block is CoffeeTableBlock
+open class CoffeeTableBlock(variant: BlockVariant) : Block(variant.createSettings().nonOpaque()), Waterloggable {
+    override fun appendProperties(builder: StateManager.Builder<Block, BlockState>) {
+        super.appendProperties(builder)
+        builder.add(WATERLOGGED)
+    }
 
-    override fun getShapeForKey(key: Byte): VoxelShape = SHAPE
+    override fun getPlacementState(context: ItemPlacementContext) =
+        super.getPlacementState(context)?.with(
+            Properties.WATERLOGGED,
+            context.world.getFluidState(context.blockPos).fluid == Fluids.WATER
+        )
 
-    override fun isCarpetingEnabled() = false
+    override fun getFluidState(state: BlockState) =
+        if (state[Properties.WATERLOGGED]) Fluids.WATER.getStill(false)
+        else super.getFluidState(state)
 
-    override fun isSittingEnabled() = false
+    override fun getOutlineShape(state: BlockState, view: BlockView, pos: BlockPos, ePos: EntityContext): VoxelShape =
+        SHAPE
 
     override fun isSimpleFullBlock(state: BlockState?, view: BlockView?, pos: BlockPos?) = false
 
     companion object {
-
+        val WATERLOGGED: BooleanProperty = Properties.WATERLOGGED
         private val SHAPE = createCuboidShape(0.0, 0.0, 0.0, 16.0, 12.0, 16.0)
-        //private val SHAPES: Byte2ObjectMap<VoxelShape>
-
-        init {
-            /*val topShape = createCuboidShape(0.0, 10.0, 0.0, 16.0, 12.0, 16.0)
-            val legX0Z0 = createCuboidShape(1.0, 0.0, 1.0, 4.0, 10.0, 4.0)
-            val legX1Z0 = createCuboidShape(12.0, 0.0, 1.0, 15.0, 10.0, 4.0)
-            val legX0Z1 = createCuboidShape(1.0, 0.0, 12.0, 4.0, 10.0, 15.0)
-            val legX1Z1 = createCuboidShape(12.0, 0.0, 12.0, 15.0, 10.0, 15.0)
-            val booleans = setOf(true, false)
-
-            fun makeShape(
-                north: Boolean,
-                east: Boolean,
-                south: Boolean,
-                west: Boolean
-            ): VoxelShape {
-                val parts = arrayListOf<VoxelShape?>(topShape)
-
-                if (north || east || south || west) {
-                    val trueCount = booleanArrayOf(north, east, south, west).count { it }
-
-                    if (trueCount == 2) {
-                        // Corners
-                        parts += when {
-                            north && west -> legX1Z1
-                            north && east -> legX0Z1
-                            south && west -> legX1Z0
-                            south && east -> legX0Z0
-                            else -> null // Straight lines ignored
-                        }
-                    } else if (trueCount == 1) {
-                        // Ends
-                        when {
-                            north -> {
-                                parts += legX0Z1
-                                parts += legX1Z1
-                            }
-
-                            south -> {
-                                parts += legX0Z0
-                                parts += legX1Z0
-                            }
-
-                            east -> {
-                                parts += legX0Z0
-                                parts += legX0Z1
-                            }
-
-                            west -> {
-                                parts += legX1Z0
-                                parts += legX1Z1
-                            }
-                        }
-                    }
-                } else {
-                    // No connections = all legs
-                    parts += legX0Z0
-                    parts += legX1Z0
-                    parts += legX0Z1
-                    parts += legX1Z1
-                }
-
-                return parts.filterNotNull().reduce(VoxelShapes::union)
-            }
-
-            SHAPES = Byte2ObjectOpenHashMap(run {
-                booleans.flatMap { north ->
-                    booleans.flatMap { east ->
-                        booleans.flatMap { south ->
-                            booleans.map { west ->
-                                Bits.buildCoffeeTableState(north, east, south, west) to
-                                    makeShape(north, east, south, west)
-                            }
-                        }
-                    }
-                }.toMap()
-            })*/
-
-        }
     }
 }
