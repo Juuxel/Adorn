f592eac0e3f001753b6178dd2d213088edf57261
Juuxel <6596629+Juuxel@users.noreply.github.com>
2020-01-18 18:11:22 +0200

    Add more chain link fence sounds, improve textures

--- src/main/kotlin/juuxel/adorn/lib/AdornSounds.kt
+++ src/main/kotlin/juuxel/adorn/lib/AdornSounds.kt
@@ -3,20 +3,23 @@ package juuxel.adorn.lib
 import juuxel.adorn.Adorn
 import net.minecraft.sound.BlockSoundGroup
 import net.minecraft.sound.SoundEvent
-import net.minecraft.sound.SoundEvents
 import net.minecraft.util.registry.Registry
 
 object AdornSounds {
+    val BLOCK_CHAIN_LINK_FENCE_BREAK = register("block.adorn.chain_link_fence.break")
     val BLOCK_CHAIN_LINK_FENCE_STEP = register("block.adorn.chain_link_fence.step")
+    val BLOCK_CHAIN_LINK_FENCE_PLACE = register("block.adorn.chain_link_fence.place")
+    val BLOCK_CHAIN_LINK_FENCE_HIT = register("block.adorn.chain_link_fence.hit")
+    val BLOCK_CHAIN_LINK_FENCE_FALL = register("block.adorn.chain_link_fence.fall")
 
     val CHAIN_LINK_FENCE = BlockSoundGroup(
         1.0F,
         1.5F,
-        SoundEvents.BLOCK_METAL_BREAK,
+        BLOCK_CHAIN_LINK_FENCE_BREAK,
         BLOCK_CHAIN_LINK_FENCE_STEP,
-        SoundEvents.BLOCK_METAL_PLACE,
-        SoundEvents.BLOCK_METAL_HIT,
-        SoundEvents.BLOCK_METAL_FALL
+        BLOCK_CHAIN_LINK_FENCE_PLACE,
+        BLOCK_CHAIN_LINK_FENCE_HIT,
+        BLOCK_CHAIN_LINK_FENCE_FALL
     )
 
     fun init() {}
