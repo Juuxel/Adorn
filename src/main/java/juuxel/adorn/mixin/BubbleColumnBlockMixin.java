1924de521242f021730392a48a39aff0530ab7db
Juuxel <6596629+Juuxel@users.noreply.github.com>
2020-05-16 01:59:01 +0300

    Fix mixin and LibGui

--- src/main/java/juuxel/adorn/mixin/BubbleColumnBlockMixin.java
+++ src/main/java/juuxel/adorn/mixin/BubbleColumnBlockMixin.java
@@ -16,14 +16,15 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 @Mixin(BubbleColumnBlock.class)
 public class BubbleColumnBlockMixin {
     @Inject(method = "canPlaceAt", at = @At("RETURN"), cancellable = true, locals = LocalCapture.CAPTURE_FAILHARD)
-    private void onCanPlaceAt(BlockState state, WorldView world, BlockPos pos, CallbackInfoReturnable<Boolean> info, Block downBlock) {
-        if (!info.getReturnValueZ() && downBlock instanceof PrismarineChimneyBlock.WithColumn) {
+    private void onCanPlaceAt(BlockState state, WorldView world, BlockPos pos, CallbackInfoReturnable<Boolean> info, BlockState stateBelow) {
+        if (!info.getReturnValueZ() && stateBelow.getBlock() instanceof PrismarineChimneyBlock.WithColumn) {
             info.setReturnValue(true);
         }
     }
 
     @Inject(method = "calculateDrag", at = @At("RETURN"), cancellable = true, locals = LocalCapture.CAPTURE_FAILHARD)
-    private static void onCalculateDrag(BlockView world, BlockPos pos, CallbackInfoReturnable<Boolean> info, BlockState state, Block block) {
+    private static void onCalculateDrag(BlockView world, BlockPos pos, CallbackInfoReturnable<Boolean> info, BlockState state) {
+        Block block = state.getBlock();
         if (info.getReturnValueZ() && block instanceof PrismarineChimneyBlock.WithColumn) {
             info.setReturnValue(((PrismarineChimneyBlock.WithColumn) block).getDrag());
         }
