358c6148f42e8a12ce896a215d3e01079ee8d2e4
Juuxel <6596629+Juuxel@users.noreply.github.com>
2021-01-18 13:19:26 +0200

    Update Ripple to fix a file path bug

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     java
     kotlin("jvm") version "1.4.20"
     id("io.github.juuxel.fabric-loom") version "0.6.7"
-    id("io.github.juuxel.ripple") version "0.3.5"
+    id("io.github.juuxel.ripple") version "0.3.6"
     `maven-publish`
     id("org.jmailen.kotlinter") version "3.2.0"
 }
