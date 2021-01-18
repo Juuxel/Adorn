4e49f5554109526547b14a83267e9a410d5f30a8
Juuxel <6596629+Juuxel@users.noreply.github.com>
2021-01-18 10:28:13 +0200

    Update Gradle plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,10 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     java
     kotlin("jvm") version "1.4.20"
-    idea
-    id("io.github.juuxel.fabric-loom") version "0.6.6"
+    id("io.github.juuxel.fabric-loom") version "0.6.7"
+    id("io.github.juuxel.ripple") version "0.3.5"
     `maven-publish`
     id("org.jmailen.kotlinter") version "3.2.0"
-    id("io.github.juuxel.ripple") version "0.3.2"
 }
 
 group = "io.github.juuxel"
