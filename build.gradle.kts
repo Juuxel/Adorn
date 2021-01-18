7476ac0b9851ebd8ec54c88c0635cbd6dad64307
Juuxel <6596629+Juuxel@users.noreply.github.com>
2021-01-18 08:37:48 +0200

    Update to Loom 0.6.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     java
     kotlin("jvm") version "1.4.20"
     idea
-    id("io.github.juuxel.fabric-loom") version "0.6.5"
+    id("io.github.juuxel.fabric-loom") version "0.6.6"
     `maven-publish`
     id("org.jmailen.kotlinter") version "3.2.0"
     id("io.github.juuxel.ripple") version "0.3.2"
