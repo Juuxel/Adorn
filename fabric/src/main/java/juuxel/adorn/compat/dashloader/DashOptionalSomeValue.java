65e72a7351e53ed4c5bac6963c1ff3efd242b506
Juuz <6596629+Juuxel@users.noreply.github.com>
2021-07-05 20:56:05 +0300

    Fix DashOptionalSomeValue

--- fabric/src/main/java/juuxel/adorn/compat/dashloader/DashOptionalSomeValue.java
+++ fabric/src/main/java/juuxel/adorn/compat/dashloader/DashOptionalSomeValue.java
@@ -4,30 +4,37 @@ import io.activej.serializer.annotations.Deserialize;
 import io.activej.serializer.annotations.Serialize;
 import juuxel.adorn.block.property.OptionalProperty;
 import net.oskarstrom.dashloader.DashRegistry;
-import net.oskarstrom.dashloader.api.ExtraVariables;
 import net.oskarstrom.dashloader.api.annotation.DashConstructor;
 import net.oskarstrom.dashloader.api.annotation.DashObject;
 import net.oskarstrom.dashloader.api.enums.ConstructorMode;
-import net.oskarstrom.dashloader.blockstate.property.value.DashEnumValue;
 import net.oskarstrom.dashloader.blockstate.property.value.DashPropertyValue;
 
 @DashObject(OptionalProperty.Value.Some.class)
 public class DashOptionalSomeValue implements DashPropertyValue {
     @Serialize(order = 0)
-    public final DashEnumValue value;
+    public final String name;
+    @Serialize(order = 1)
+    public final String enumClass;
 
-    public DashOptionalSomeValue(@Deserialize("value") DashEnumValue value) {
-        this.value = value;
+    public DashOptionalSomeValue(@Deserialize("name") String name, @Deserialize("enumClass") String enumClass) {
+        this.name = name;
+        this.enumClass = enumClass;
     }
 
-    @DashConstructor(ConstructorMode.OBJECT_EXTRA)
-    public DashOptionalSomeValue(OptionalProperty.Value.Some<?> value, ExtraVariables extra) {
-        this(new DashEnumValue(value.getValue(), extra));
+    @DashConstructor(ConstructorMode.OBJECT)
+    public DashOptionalSomeValue(OptionalProperty.Value.Some<?> value) {
+        this(value.getValue().name(), value.getValue().getClass().getName());
     }
 
     @SuppressWarnings({"rawtypes", "unchecked"})
     @Override
     public Comparable<?> toUndash(DashRegistry registry) {
-        return new OptionalProperty.Value.Some(value.toUndash(registry));
+        try {
+            Class c = Class.forName(enumClass);
+            Enum<?> e = Enum.valueOf(c, name);
+            return new OptionalProperty.Value.Some(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
