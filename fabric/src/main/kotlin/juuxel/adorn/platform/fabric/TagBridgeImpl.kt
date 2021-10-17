41eed9999917753e4416ce766efc0b88a70c0e85
Juuz <6596629+Juuxel@users.noreply.github.com>
2021-10-18 01:12:21 +0300

    fabric: Replace deprecated TagRegistry with TagFactory

--- fabric/src/main/kotlin/juuxel/adorn/platform/fabric/TagBridgeImpl.kt
+++ fabric/src/main/kotlin/juuxel/adorn/platform/fabric/TagBridgeImpl.kt
@@ -1,7 +1,7 @@
 package juuxel.adorn.platform.fabric
 
 import juuxel.adorn.platform.TagBridge
-import net.fabricmc.fabric.api.tag.TagRegistry
+import net.fabricmc.fabric.api.tag.TagFactory
 import net.minecraft.block.Block
 import net.minecraft.item.Item
 import net.minecraft.tag.Tag
@@ -9,8 +9,8 @@ import net.minecraft.util.Identifier
 
 object TagBridgeImpl : TagBridge {
     override fun block(id: Identifier): Tag<Block> =
-        TagRegistry.block(id)
+        TagFactory.BLOCK.create(id)
 
     override fun item(id: Identifier): Tag<Item> =
-        TagRegistry.item(id)
+        TagFactory.ITEM.create(id)
 }
