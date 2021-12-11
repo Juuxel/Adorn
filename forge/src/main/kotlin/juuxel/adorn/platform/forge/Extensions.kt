9bb88359dc71faa23ab0a6b4bed89faca05ece79
Juuz <6596629+Juuxel@users.noreply.github.com>
2021-12-11 13:49:34 +0200

    forge: Remove unused Extensions.kt

--- forge/src/main/kotlin/juuxel/adorn/platform/forge/Extensions.kt
+++
@@ -1,7 +0,0 @@
-package juuxel.adorn.platform.forge
-
-import juuxel.adorn.platform.Registrar
-import net.minecraftforge.eventbus.api.IEventBus
-
-fun Registrar<*>.register(modBus: IEventBus): Unit =
-    (this as RegistrarImpl).register.register(modBus)
