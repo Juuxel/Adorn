a6073395cdf4d2ba3a46bd195c8adde731729455
Juuz <6596629+Juuxel@users.noreply.github.com>
2022-03-31 13:46:07 +0300

    Fix game rule default values from the config not being used

--- common/src/main/kotlin/juuxel/adorn/lib/AdornGameRules.kt
+++ common/src/main/kotlin/juuxel/adorn/lib/AdornGameRules.kt
@@ -1,6 +1,8 @@
 package juuxel.adorn.lib
 
 import juuxel.adorn.AdornCommon
+import juuxel.adorn.config.Config
+import juuxel.adorn.platform.PlatformBridges
 import net.minecraft.world.GameRules
 import net.minecraft.world.GameRules.BooleanRule
 import net.minecraft.world.GameRules.Category
@@ -10,14 +12,17 @@ import net.minecraft.world.GameRules.Type
 
 object AdornGameRules {
     @JvmField
-    val SKIP_NIGHT_ON_SOFAS: Key<BooleanRule> = register("skipNightOnSofas", Category.PLAYER, BooleanRule.create(true))
+    val SKIP_NIGHT_ON_SOFAS: Key<BooleanRule> = register("skipNightOnSofas", Category.PLAYER, createBooleanRule { it.skipNightOnSofas })
 
     @JvmField
-    val INFINITE_KITCHEN_SINKS: Key<BooleanRule> = register("infiniteKitchenSinks", Category.MISC, BooleanRule.create(true))
+    val INFINITE_KITCHEN_SINKS: Key<BooleanRule> = register("infiniteKitchenSinks", Category.MISC, createBooleanRule { it.infiniteKitchenSinks })
 
     @JvmStatic
     fun init() {
     }
+
+    private inline fun createBooleanRule(default: (Config.GameRuleDefaults) -> Boolean): Type<BooleanRule> =
+        BooleanRule.create(default(PlatformBridges.configManager.config.gameRuleDefaults))
 
     // <T extends Rule<T>> Key<T> register(String name, Category category, Type<T> type)
     private fun <T : Rule<T>> register(name: String, category: Category, type: Type<T>): Key<T> =
