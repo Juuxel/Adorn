77b83d5112b67b08412a5045fead8557c951ce3f
Juuz <6596629+Juuxel@users.noreply.github.com>
2022-04-21 16:57:22 +0300

    data: Document Id and add validation

--- datagen/src/main/kotlin/juuxel/adorn/datagen/Id.kt
+++ datagen/src/main/kotlin/juuxel/adorn/datagen/Id.kt
@@ -2,8 +2,27 @@ package juuxel.adorn.datagen
 
 import java.io.Serializable
 
+/**
+ * A namespaced ID. Mimics `net.minecraft.util.Identifier`.
+ */
 data class Id(val namespace: String, val path: String) : Serializable {
-    fun rawSuffixed(suffix: String): Id = Id(namespace, "$path$suffix")
+    init {
+        require(validateIdCharacters(namespace, isPath = false)) {
+            "ID namespace '$namespace' contains characters not matching [a-z0-9.-_]"
+        }
+        require(validateIdCharacters(path, isPath = true)) {
+            "ID path '$path' contains characters not matching [a-z0-9.-_/]"
+        }
+    }
+
+    /**
+     * Suffixes the path with a [suffix] without separation.
+     */
+    fun rawSuffixed(suffix: String): Id = Id(namespace, path + suffix)
+
+    /**
+     * Suffixes the path with an underscore-separated [suffix].
+     */
     fun suffixed(suffix: String): Id = rawSuffixed("_$suffix")
 
     override fun toString() = "$namespace:$path"
@@ -16,5 +35,10 @@ data class Id(val namespace: String, val path: String) : Serializable {
             val (ns, path) = parts
             return Id(ns, path)
         }
+
+        private fun validateIdCharacters(component: String, isPath: Boolean): Boolean =
+            component.all {
+                it in 'a'..'z' || it in '0'..'9' || it == '.' || it == '-' || it == '_' || (isPath && it == '/')
+            }
     }
 }
