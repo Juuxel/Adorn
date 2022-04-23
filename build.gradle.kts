90be5754cb01a6e9a3878b8fc068aada69b6176b
Juuz <6596629+Juuxel@users.noreply.github.com>
2022-04-23 14:17:45 +0300

    Update KFF to 1.17.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     java
-    kotlin("jvm") version "1.4.20"
+    kotlin("jvm") version "1.6.10"
     id("dev.architectury.loom") version "0.11.0-SNAPSHOT"
     `maven-publish`
     id("org.jmailen.kotlinter") version "3.2.0"
