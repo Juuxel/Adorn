54b0bb58594246dd49f92ec31af4dfd488a06f8d
Juuxel <6596629+Juuxel@users.noreply.github.com>
2022-06-21 13:35:08 +0300

    emi: Less cursed fluid stack handling

--- fabric/src/main/kotlin/juuxel/adorn/compat/emi/FluidSlotWidget.kt
+++ fabric/src/main/kotlin/juuxel/adorn/compat/emi/FluidSlotWidget.kt
@@ -1,12 +1,12 @@
 package juuxel.adorn.compat.emi
 
 import dev.emi.emi.api.stack.EmiIngredient
-import dev.emi.emi.api.stack.FluidEmiStack
 import dev.emi.emi.api.widget.Bounds
 import dev.emi.emi.api.widget.SlotWidget
 import juuxel.adorn.client.gui.screen.BrewerScreen
 import juuxel.adorn.fluid.FluidUnit
 import juuxel.adorn.fluid.FluidVolume
+import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariant
 import net.minecraft.client.util.math.MatrixStack
 
 class FluidSlotWidget(
@@ -15,9 +15,12 @@ class FluidSlotWidget(
     private val width: Int, private val height: Int,
 ) : SlotWidget(stack, x, y) {
     private val stacks = stack.emiStacks.map {
-        val fluidStack = it as? FluidEmiStack ?: throw IllegalArgumentException("FluidSlotWidget must only get fluid stacks!")
-        val variant = (fluidStack.entry as FluidEmiStack.FluidEntry).value
-        FluidVolume(variant.fluid, fluidStack.amount, variant.nbt, FluidUnit.DROPLET)
+        val entry = it.entry
+        require(FluidVariant::class.java.isAssignableFrom(entry.type)) {
+            "All stacks of ingredient $stack should be FluidVariants, but found $it with entry type ${entry.type}"
+        }
+        val variant = entry.value as FluidVariant
+        FluidVolume(variant.fluid, it.amount, variant.nbt, FluidUnit.DROPLET)
     }
 
     override fun getBounds(): Bounds = Bounds(x, y, width, height)
