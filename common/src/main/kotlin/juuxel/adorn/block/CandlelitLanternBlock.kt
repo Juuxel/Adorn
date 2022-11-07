9699913a6406038c084f7f2a3b4076dda81a7487
Juuz <6596629+Juuxel@users.noreply.github.com>
2022-11-07 21:01:45 +0200

    Add description and advancement for candlelit lanterns
    Also updated adorn:everything to require candlelit lanterns.

--- common/src/main/kotlin/juuxel/adorn/block/CandlelitLanternBlock.kt
+++ common/src/main/kotlin/juuxel/adorn/block/CandlelitLanternBlock.kt
@@ -10,7 +10,9 @@ import net.minecraft.util.math.Vec3d
 import net.minecraft.util.math.random.Random
 import net.minecraft.world.World
 
-class CandlelitLanternBlock(settings: Settings) : LanternBlock(settings) {
+class CandlelitLanternBlock(settings: Settings) : LanternBlock(settings), BlockWithDescription {
+    override val descriptionKey = "block.adorn.candlelit_lantern.description"
+
     override fun randomDisplayTick(state: BlockState, world: World, pos: BlockPos, random: Random) {
         val px = 1 / 16.0
         val vec = Vec3d.ofCenter(pos, if (state[HANGING]) 6 * px else 5 * px)
