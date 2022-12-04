ffe8ec538dc4a2fe28d19725d41c535fdce3349f
Juuz <6596629+Juuxel@users.noreply.github.com>
2022-12-04 20:55:37 +0200

    Remove test file

--- fabric/src/main/kotlin/juuxel/adorn/WorldsSmallestViolin.kt
+++
@@ -1,9 +0,0 @@
-package juuxel.adorn
-
-import juuxel.adorn.platform.PlatformBridges
-import java.util.ServiceLoader
-
-fun foo() {
-    val foo = ServiceLoader.load(PlatformBridges::class.java).findFirst()
-    println(foo.get())
-}
