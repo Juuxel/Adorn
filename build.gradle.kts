ffb5ac76a85604e881625c9cf3e23eb5bf19369b
Juuz <6596629+Juuxel@users.noreply.github.com>
2022-12-04 21:41:55 +0200

    Update to Loom 1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
     kotlin("jvm") version "1.7.0" apply false
 
     id("architectury-plugin") version "3.4.143"
-    id("dev.architectury.loom") version "0.12.0.282" apply false
+    id("dev.architectury.loom") version "1.0.302" apply false
     id("io.github.juuxel.loom-quiltflower") version "1.7.2" apply false
 
     id("org.jmailen.kotlinter") version "3.2.0" apply false
