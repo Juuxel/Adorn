8b3082c45abf2558ac895142c1a5c6b29c0eef0e
Juuz <6596629+Juuxel@users.noreply.github.com>
2022-12-08 00:11:17 +0200

    build-logic: Move datagen logic (excluding EMI) to datagen module

--- build-logic/src/main/kotlin/juuxel/adorn/datagen/gradle/GenerateData.kt
+++ build-logic/src/main/kotlin/juuxel/adorn/datagen/gradle/GenerateData.kt
@@ -1,11 +1,6 @@
 package juuxel.adorn.datagen.gradle
 
-import juuxel.adorn.datagen.Generator
-import juuxel.adorn.datagen.GeneratorConfig
-import juuxel.adorn.datagen.GeneratorConfigLoader
-import juuxel.adorn.datagen.Material
-import juuxel.adorn.datagen.TemplateApplier
-import juuxel.adorn.datagen.buildSubstitutions
+import juuxel.adorn.datagen.DataGenerator
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.ConfigurableFileCollection
 import org.gradle.api.file.DirectoryProperty
@@ -15,8 +10,6 @@ import org.gradle.api.tasks.TaskAction
 import org.gradle.workers.WorkAction
 import org.gradle.workers.WorkParameters
 import org.gradle.workers.WorkerExecutor
-import java.nio.file.Files
-import java.nio.file.Path
 import javax.inject.Inject
 
 abstract class GenerateData : DefaultTask() {
@@ -44,103 +37,10 @@ abstract class GenerateData : DefaultTask() {
 
     abstract class GenerationAction : WorkAction<GenerationParameters> {
         override fun execute() {
-            val outputPath = parameters.output.get().asFile.toPath()
-
-            if (Files.exists(outputPath)) {
-                // See https://stackoverflow.com/a/35989142
-                Files.walk(outputPath).use {
-                    it.sorted(Comparator.reverseOrder()).forEach { child -> Files.delete(child) }
-                }
-            }
-
-            val cache = TemplateCache()
-
-            for (configFile in parameters.configs) {
-                generate(outputPath, GeneratorConfigLoader.read(configFile.toPath()), cache)
-            }
-        }
-    }
-
-    companion object {
-        private fun generate(outputPath: Path, config: GeneratorConfig, cache: TemplateCache) {
-            val stoneMaterials = config.stones
-            generate(outputPath, Generator.STONE_GENERATORS, stoneMaterials, cache, config)
-            generate(outputPath, Generator.SIDED_STONE_GENERATORS, stoneMaterials.filter { it.material.hasSidedTexture }, cache, config)
-            generate(
-                outputPath,
-                Generator.UNSIDED_STONE_GENERATORS,
-                stoneMaterials.filter { !it.material.hasSidedTexture },
-                cache,
-                config
+            DataGenerator.generate(
+                configFiles = parameters.configs.map { it.toPath() },
+                outputPath = parameters.output.get().asFile.toPath()
             )
-            generate(outputPath, Generator.WOOD_GENERATORS, config.woods, cache, config)
-            generate(outputPath, Generator.WOOL_GENERATORS, config.wools, cache, config)
         }
-
-        private fun <M : Material> generate(
-            outputPath: Path,
-            gens: List<Generator<M>>,
-            mats: Iterable<GeneratorConfig.MaterialEntry<M>>,
-            templateCache: TemplateCache,
-            config: GeneratorConfig,
-        ) {
-            val conditionType = config.conditionType
-            for (gen in gens) {
-                val templateText = templateCache.load(gen.templatePath)
-
-                for ((mat, exclude, replace) in mats) {
-                    if (gen.id in exclude) continue
-
-                    val mainSubstitutions = buildSubstitutions {
-                        "wood_texture_separator" with "_"
-                        "advancement-condition" with "<load-condition>"
-                        "loot-table-condition" with "<load-condition>"
-                        "recipe-condition" with "<load-condition>"
-                        "load-condition" with ""
-
-                        for ((type, path) in conditionType.conditionsInFileTemplatePathsByType) {
-                            "$type-condition" with templateCache.load(path)
-                        }
-
-                        putAll(config.rootReplacements)
-                        init(mat)
-                        gen.substitutionConfig(this, mat)
-                        putAll(replace)
-                    }
-                    val output = TemplateApplier.apply(templateText, mainSubstitutions)
-                    val filePathStr = TemplateApplier.apply(gen.outputPathTemplate, mainSubstitutions)
-                    val filePath = outputPath.resolve(filePathStr)
-                    Files.createDirectories(filePath.parent)
-                    Files.writeString(filePath, output)
-
-                    if (gen.requiresCondition && mat.isModded()) {
-                        val externalConditionPathTemplate = conditionType.separateFilePathTemplate
-                        if (externalConditionPathTemplate != null) {
-                            val conditionTemplate = templateCache.load(conditionType.separateFileTemplatePath!!)
-                            val conditionSubstitutions = buildSubstitutions {
-                                "mod-id" with mat.id.namespace
-                                "file-path" with filePathStr
-                            }
-                            val conditionText = TemplateApplier.apply(conditionTemplate, conditionSubstitutions)
-                            val conditionPathStr = TemplateApplier.apply(externalConditionPathTemplate, conditionSubstitutions)
-                            val conditionPath = outputPath.resolve(conditionPathStr)
-                            Files.createDirectories(conditionPath.parent)
-                            Files.writeString(conditionPath, conditionText)
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    private class TemplateCache {
-        private val cache = HashMap<String, String>()
-
-        fun load(path: String): String =
-            cache.getOrPut(path) {
-                GenerateData::class.java.getResourceAsStream("/adorn/templates/$path").use {
-                    it!!.bufferedReader().readText()
-                }
-            }
     }
 }
---
+++ datagen/src/main/kotlin/juuxel/adorn/datagen/DataGenerator.kt
@@ -0,0 +1,102 @@
+package juuxel.adorn.datagen
+
+import java.nio.file.Files
+import java.nio.file.Path
+
+object DataGenerator {
+    fun generate(configFiles: List<Path>, outputPath: Path) {
+        if (Files.exists(outputPath)) {
+            // See https://stackoverflow.com/a/35989142
+            Files.walk(outputPath).use {
+                it.sorted(Comparator.reverseOrder()).forEach { child -> Files.delete(child) }
+            }
+        }
+
+        val cache = TemplateCache()
+
+        for (configFile in configFiles) {
+            generate(outputPath, GeneratorConfigLoader.read(configFile), cache)
+        }
+    }
+
+    private fun generate(outputPath: Path, config: GeneratorConfig, cache: TemplateCache) {
+        val stoneMaterials = config.stones
+        generate(outputPath, Generator.STONE_GENERATORS, stoneMaterials, cache, config)
+        generate(outputPath, Generator.SIDED_STONE_GENERATORS, stoneMaterials.filter { it.material.hasSidedTexture }, cache, config)
+        generate(
+            outputPath,
+            Generator.UNSIDED_STONE_GENERATORS,
+            stoneMaterials.filter { !it.material.hasSidedTexture },
+            cache,
+            config
+        )
+        generate(outputPath, Generator.WOOD_GENERATORS, config.woods, cache, config)
+        generate(outputPath, Generator.WOOL_GENERATORS, config.wools, cache, config)
+    }
+
+    private fun <M : Material> generate(
+        outputPath: Path,
+        gens: List<Generator<M>>,
+        mats: Iterable<GeneratorConfig.MaterialEntry<M>>,
+        templateCache: TemplateCache,
+        config: GeneratorConfig,
+    ) {
+        val conditionType = config.conditionType
+        for (gen in gens) {
+            val templateText = templateCache.load(gen.templatePath)
+
+            for ((mat, exclude, replace) in mats) {
+                if (gen.id in exclude) continue
+
+                val mainSubstitutions = buildSubstitutions {
+                    "wood_texture_separator" with "_"
+                    "advancement-condition" with "<load-condition>"
+                    "loot-table-condition" with "<load-condition>"
+                    "recipe-condition" with "<load-condition>"
+                    "load-condition" with ""
+
+                    for ((type, path) in conditionType.conditionsInFileTemplatePathsByType) {
+                        "$type-condition" with templateCache.load(path)
+                    }
+
+                    putAll(config.rootReplacements)
+                    init(mat)
+                    gen.substitutionConfig(this, mat)
+                    putAll(replace)
+                }
+                val output = TemplateApplier.apply(templateText, mainSubstitutions)
+                val filePathStr = TemplateApplier.apply(gen.outputPathTemplate, mainSubstitutions)
+                val filePath = outputPath.resolve(filePathStr)
+                Files.createDirectories(filePath.parent)
+                Files.writeString(filePath, output)
+
+                if (gen.requiresCondition && mat.isModded()) {
+                    val externalConditionPathTemplate = conditionType.separateFilePathTemplate
+                    if (externalConditionPathTemplate != null) {
+                        val conditionTemplate = templateCache.load(conditionType.separateFileTemplatePath!!)
+                        val conditionSubstitutions = buildSubstitutions {
+                            "mod-id" with mat.id.namespace
+                            "file-path" with filePathStr
+                        }
+                        val conditionText = TemplateApplier.apply(conditionTemplate, conditionSubstitutions)
+                        val conditionPathStr = TemplateApplier.apply(externalConditionPathTemplate, conditionSubstitutions)
+                        val conditionPath = outputPath.resolve(conditionPathStr)
+                        Files.createDirectories(conditionPath.parent)
+                        Files.writeString(conditionPath, conditionText)
+                    }
+                }
+            }
+        }
+    }
+
+    private class TemplateCache {
+        private val cache = HashMap<String, String>()
+
+        fun load(path: String): String =
+            cache.getOrPut(path) {
+                DataGenerator::class.java.getResourceAsStream("/adorn/templates/$path").use {
+                    it!!.bufferedReader().readText()
+                }
+            }
+    }
+}
