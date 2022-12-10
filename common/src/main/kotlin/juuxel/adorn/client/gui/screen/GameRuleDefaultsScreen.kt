ba575b9787b4e21b02217135a27ce9a8a26dee4e
Juuz <6596629+Juuxel@users.noreply.github.com>
2022-12-11 00:57:40 +0200

    Simplify config screen layouts in code

--- common/src/main/kotlin/juuxel/adorn/client/gui/screen/GameRuleDefaultsScreen.kt
+++ common/src/main/kotlin/juuxel/adorn/client/gui/screen/GameRuleDefaultsScreen.kt
@@ -10,13 +10,9 @@ class GameRuleDefaultsScreen(parent: Screen) : AbstractConfigScreen(Text.transla
     override fun init() {
         super.init()
         val config = ConfigManager.config()
-        val x = (width - BUTTON_WIDTH) / 2
-        var nextY = 40
-        addDrawableChild(createConfigToggle(x, nextY, BUTTON_WIDTH, config.gameRuleDefaults::skipNightOnSofas))
-        nextY += BUTTON_SPACING
-        addDrawableChild(createConfigToggle(x, nextY, BUTTON_WIDTH, config.gameRuleDefaults::infiniteKitchenSinks))
-        nextY += BUTTON_SPACING
-        addDrawableChild(createConfigToggle(x, nextY, BUTTON_WIDTH, config.gameRuleDefaults::dropLockedTradingStations))
+        addConfigToggle(BUTTON_WIDTH, config.gameRuleDefaults::skipNightOnSofas)
+        addConfigToggle(BUTTON_WIDTH, config.gameRuleDefaults::infiniteKitchenSinks)
+        addConfigToggle(BUTTON_WIDTH, config.gameRuleDefaults::dropLockedTradingStations)
         addDrawableChild(ButtonWidget(this.width / 2 - 100, this.height - 27, 200, 20, ScreenTexts.BACK) { close() })
     }
 
--- common/src/main/kotlin/juuxel/adorn/client/gui/screen/MainConfigScreen.kt
+++ common/src/main/kotlin/juuxel/adorn/client/gui/screen/MainConfigScreen.kt
@@ -1,6 +1,5 @@
 package juuxel.adorn.client.gui.screen
 
-import juuxel.adorn.client.gui.widget.ConfigScreenHeading
 import juuxel.adorn.config.ConfigManager
 import juuxel.adorn.fluid.FluidUnit
 import net.minecraft.client.gui.screen.Screen
@@ -13,19 +12,13 @@ class MainConfigScreen(parent: Screen) : AbstractConfigScreen(Text.translatable(
         super.init()
         val config = ConfigManager.config()
         val x = (width - BUTTON_WIDTH) / 2
-        var nextY = 40
-        addDrawable(ConfigScreenHeading(Text.translatable("gui.adorn.config.visual"), x, nextY, BUTTON_WIDTH))
-        nextY += ConfigScreenHeading.HEIGHT
-        addDrawableChild(createConfigToggle(x, nextY, BUTTON_WIDTH, config.client::showTradingStationTooltips))
-        nextY += BUTTON_SPACING
-        addDrawableChild(createConfigToggle(x, nextY, BUTTON_WIDTH, config.client::showItemsInStandardGroups))
-        nextY += BUTTON_SPACING
-        addDrawableChild(createConfigButton(x, nextY, BUTTON_WIDTH, config.client::displayedFluidUnit, FluidUnit.values().toList()))
-        nextY += BUTTON_SPACING
-        addDrawable(ConfigScreenHeading(Text.translatable("gui.adorn.config.other"), x, nextY, BUTTON_WIDTH))
-        nextY += ConfigScreenHeading.HEIGHT
+        addHeading(Text.translatable("gui.adorn.config.visual"), BUTTON_WIDTH)
+        addConfigToggle(BUTTON_WIDTH, config.client::showTradingStationTooltips)
+        addConfigToggle(BUTTON_WIDTH, config.client::showItemsInStandardGroups)
+        addConfigButton(BUTTON_WIDTH, config.client::displayedFluidUnit, FluidUnit.values().toList())
+        addHeading(Text.translatable("gui.adorn.config.other"), BUTTON_WIDTH)
         addDrawableChild(
-            ButtonWidget(x, nextY, BUTTON_WIDTH, 20, Text.translatable("gui.adorn.config.game_rule_defaults")) {
+            ButtonWidget(x, nextChildY, BUTTON_WIDTH, 20, Text.translatable("gui.adorn.config.game_rule_defaults")) {
                 client!!.setScreen(GameRuleDefaultsScreen(this))
             }
         )
@@ -33,7 +26,6 @@ class MainConfigScreen(parent: Screen) : AbstractConfigScreen(Text.translatable(
     }
 
     companion object {
-        private const val HEADING_HEIGHT = 12
         private const val BUTTON_WIDTH = 200
     }
 }
