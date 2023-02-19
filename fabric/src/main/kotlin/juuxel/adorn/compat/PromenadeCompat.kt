58238798576c3934fbd5cc428cf99680d0211530
Juuz <6596629+Juuxel@users.noreply.github.com>
2023-02-19 16:34:12 +0200

    Update Promenade compat to support 3.1.0
    - Cherry oak -> sakura (Fixes #303)
    - Update texture paths

--- fabric/src/main/kotlin/juuxel/adorn/compat/PromenadeCompat.kt
+++ fabric/src/main/kotlin/juuxel/adorn/compat/PromenadeCompat.kt
@@ -5,8 +5,8 @@ import juuxel.adorn.block.variant.BlockVariantSet
 
 object PromenadeCompat : BlockVariantSet {
     override val woodVariants = listOf(
-        "cherry_oak",
         "dark_amaranth",
         "palm",
+        "sakura",
     ).map { BlockVariant.Wood("promenade/$it") }
 }
