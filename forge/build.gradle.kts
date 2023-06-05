11308dd72ef38d80e079560d9d931db29af021cc
Juuz <6596629+Juuxel@users.noreply.github.com>
2023-06-05 13:34:52 +0300

    forge: Add back needed dependencies lost in merge

--- forge/build.gradle.kts
+++ forge/build.gradle.kts
@@ -48,6 +48,9 @@ dependencies {
     // Add Kotlin for Forge.
     // Based on their own instructions: https://github.com/thedarkcolour/KotlinForForge/blob/70385f5/thedarkcolour/kotlinforforge/gradle/kff-3.0.0.gradle
     implementation("thedarkcolour:kotlinforforge:${rootProject.property("kotlin-for-forge")}")
+    // Without the manually specified versions, Loom's generateDLIConfig fails??
+    forgeRuntimeLibrary(kotlin("stdlib-jdk8", version = "1.6.0"))
+    forgeRuntimeLibrary(kotlin("reflect", version = "1.6.0"))
 
     // Depend on the common project. The "namedElements" configuration contains the non-remapped
     // classes and resources of the project.
