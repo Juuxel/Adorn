cee4a1c8496559dd6b7eb6cc478e08c6181f53f9
Juuz <6596629+Juuxel@users.noreply.github.com>
2025-05-18 15:19:15 +0300

    common: Fix fall location for stone ladders (used for fall death messages)

---
+++ common/src/main/java/juuxel/adorn/commonmixin/FallLocationMixin.java
@@ -0,0 +1,19 @@
+package juuxel.adorn.commonmixin;
+
+import juuxel.adorn.block.AdornBlocks;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.damage.FallLocation;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(FallLocation.class)
+abstract class FallLocationMixin {
+    @Inject(method = "fromBlockState", at = @At("HEAD"), cancellable = true)
+    private static void allowStoneLadders(BlockState state, CallbackInfoReturnable<FallLocation> info) {
+        if (state.isOf(AdornBlocks.STONE_LADDER.get())) {
+            info.setReturnValue(FallLocation.LADDER);
+        }
+    }
+}
