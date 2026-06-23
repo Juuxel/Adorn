b3e3351cc751ff122791aa5620ff9dddfa6a84eb
Juuz <6596629+Juuxel@users.noreply.github.com>
2026-06-23 19:15:39 +0300

    build: Use data configs as task inputs

--- build-logic/src/main/java/juuxel/adorn/gradle/datagen/DataConfig.java
+++ build-logic/src/main/java/juuxel/adorn/gradle/datagen/DataConfig.java
@@ -3,12 +3,23 @@ package juuxel.adorn.gradle.datagen;
 import org.gradle.api.Named;
 import org.gradle.api.file.ConfigurableFileCollection;
 import org.gradle.api.provider.Property;
+import org.gradle.api.tasks.Input;
+import org.gradle.api.tasks.InputFiles;
+import org.gradle.api.tasks.Internal;
 
 import javax.inject.Inject;
 
 public abstract class DataConfig implements Named {
+    @InputFiles
     public abstract ConfigurableFileCollection getFiles();
+
+    @Input
     public abstract Property<Boolean> getTagsOnly();
+
+    // Override to add @Internal task input annotation
+    @Internal
+    @Override
+    public abstract String getName();
 
     @Inject
     public DataConfig() {
