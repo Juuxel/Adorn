b3003b61a8cfcaf39aa2eeee00c941901890a714
Juuz <6596629+Juuxel@users.noreply.github.com>
2026-06-24 21:29:50 +0300

    ci: Rewrite git status check

--- build-logic/check_and_package_git_diff.py
+++ build-logic/check_and_package_git_diff.py
@@ -1,21 +1,17 @@
-from os import remove
-from sys import stdin
+import subprocess
 from zipfile import ZipFile
 
+process = subprocess.run(["git", "status", "--porcelain"], capture_output=True, text=True)
 
-written = False
+if process.stdout == "":
+    exit(0)
 
 with ZipFile("diff.zip", "w") as zip:
-    for line in stdin:
-        line = line.removesuffix("\n")
+    for line in process.stdout.splitlines():
         print(line)
-        written = True
         status = line[0:2]
-        file = line[line.index(" -> ") + 4:] if " -> " in line else line[3:]
+        file = line[line.index(" -> ") + 4 :] if " -> " in line else line[3:]
         if status[1] != "D":
             zip.write(file)
 
-if written:
-    exit(1)
-else:
-    remove("diff.zip")
+exit(1)
