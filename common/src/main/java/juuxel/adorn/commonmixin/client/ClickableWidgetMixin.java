72153501d82a2606dc38bb17e1ccfa8f2d411771
Juuz <6596629+Juuxel@users.noreply.github.com>
2026-06-29 02:04:34 +0300

    Fix button hovering not working in scroll envelopes

--- common/src/main/java/juuxel/adorn/commonmixin/client/ClickableWidgetMixin.java
+++ common/src/main/java/juuxel/adorn/commonmixin/client/ClickableWidgetMixin.java
@@ -9,6 +9,11 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(ClickableWidget.class)
 abstract class ClickableWidgetMixin {
+    @ModifyArg(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;scissorContains(II)Z"), index = 1)
+    private int modifyYForHoverScissor(int y) {
+        return modifyYForScissor(y);
+    }
+
     @ModifyArg(method = "drawScrollableText(Lnet/minecraft/client/gui/DrawContext;Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/text/Text;IIIIII)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;enableScissor(IIII)V"), index = 1)
     private static int modifyStartYForScissor(int y) {
         return modifyYForScissor(y);
