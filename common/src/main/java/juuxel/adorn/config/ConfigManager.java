9934adfbc283aeffba6ec8269cfa1779699c5529
Juuz <6596629+Juuxel@users.noreply.github.com>
2026-06-28 23:37:20 +0300

    ConfigManager: Fix upgrading not working

--- common/src/main/java/juuxel/adorn/config/ConfigManager.java
+++ common/src/main/java/juuxel/adorn/config/ConfigManager.java
@@ -117,7 +117,7 @@ public abstract class ConfigManager {
 
             if (entry.getValue() instanceof JsonObject value) {
                 var actual = config.get(JsonObject.class, entry.getKey());
-                return actual == null || isMissingKeys(actual, value);
+                if (actual == null || isMissingKeys(actual, value)) return true;
             }
         }
 
