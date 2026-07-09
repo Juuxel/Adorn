c58c1bb091f3b925c2aad6f90138785638b7dae3
Juuz <6596629+Juuxel@users.noreply.github.com>
2026-07-09 13:54:54 +0300

    neo: Fix thread safety issue when registering game rules
    Fixes #353.

--- forge/src/main/java/juuxel/adorn/platform/forge/Adorn.java
+++ forge/src/main/java/juuxel/adorn/platform/forge/Adorn.java
@@ -87,7 +87,7 @@ public final class Adorn {
     }
 
     private void init(FMLCommonSetupEvent event) {
-        AdornGameRules.init();
+        event.enqueueWork(() -> AdornGameRules.init());
         AdornTags.init();
         AdornStats.init();
         ConfigManager.get().finish();
